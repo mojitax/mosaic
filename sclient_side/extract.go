32473ac0c0b2c35e8282f4a312f316a510505b7d
mojitax <94326860+mojitax@users.noreply.github.com>
2022-04-15 10:08:19 +0200

    Update extract.go

--- sclient_side/extract.go
+++ sclient_side/extract.go
@@ -3,25 +3,25 @@ package main
 import (
 	"bufio"
 	//"bytes"
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base32"
-	"encoding/hex"
-	"encoding/json"
-	//"io/ioutil"
-	"log"
-	"math/rand"
 	//"net/http"
+	//"io/ioutil"
+
+	"encoding/json"
+	
+	"log"
+
+	
 	"os"
-	"os/exec"
-	"time"
+
+
 	"mosaic/sclient_side/chat"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"github.com/xlzd/gotp"
-	"gopkg.in/yaml.v2"
+
 	"flag"
 	"fmt"
+	//"time"
 	//"strings"
 )
 
@@ -34,7 +34,6 @@ type Response struct {
 //Message - struct to return Key + params for specific deviceID
 type Auth struct {
 	ID      string `json:"ID"`
-	Indexes []int  `json:"indexes"`
 	TOTP    string `json:"TOTP"`
 	Attributes []string `json:"Attributes"`
 }
@@ -81,79 +80,6 @@ func FindUint(slice []int, val int) bool {
 	return false
 }
 
-//Generate_Key to OTP
-func Generate_Key() ([32]byte, []int) {
-	NumebrOfParams := 4
-	var response string
-	var paramnumbers []int
-	rand.Seed(time.Now().UnixNano())
-	for r := 0; r < NumebrOfParams; r++ {
-		newParamNumber := (rand.Intn(len(arrayOfCommands)) + 1)
-		if !FindUint(paramnumbers, newParamNumber) {
-			paramnumbers = append(paramnumbers, newParamNumber)
-		} else {
-			r--
-		}
-	}
-	for i := 0; i < (len(paramnumbers)); i++ {
-		result := exec.Command("/bin/bash", "-c", arrayOfCommands[paramnumbers[i]-1])
-		out, err := result.CombinedOutput()
-		if err != nil {
-			h := hmac.New(sha256.New, []byte(cfg.HashKey))
-			h.Write([]byte(err.Error()))
-			sha := hex.EncodeToString(h.Sum(nil))
-			response = response + sha
-		} else {
-			h := hmac.New(sha256.New, []byte(cfg.HashKey))
-			h.Write([]byte(out))
-			sha := hex.EncodeToString(h.Sum(nil))
-			response = response + sha
-		}
-	}
-	key := sha256.Sum256([]byte(response))
-	return key, paramnumbers
-}
-
-//must - Error handling
-func must(err error) {
-	if err == nil {
-		return
-	}
-	log.Panicln(err)
-}
-
-//readFile - read config file
-func readFile(cfg *Config) {
-	log.Printf("Reading config file")
-	f, err := os.Open("sclient_side/config.yaml")
-	if err != nil {
-		must(err)
-	}
-	defer f.Close()
-
-	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(cfg)
-	if err != nil {
-		must(err)
-	}
-}
-
-//readCommands - read commands
-func readCommands() {
-	log.Printf("Reading commands")
-	f, err := os.Open("sclient_side/commands.yaml")
-	if err != nil {
-		must(err)
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		arrayOfCommands = append(arrayOfCommands, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		must(err)
-	}
-}
 func readLines(path string) ([]string, error) {
     file, err := os.Open(path)
     if err != nil {
@@ -175,19 +101,21 @@ func main() {
     if len(attrsPath) == 0 {
         panic("-attrs is required")
     }
-	
+	seed, _:=readLines("/mosaic/new_files/seedkey")
+	if (len(seed)!=2){
+		log.Fatalf("Wrong seed file")
+	}
+	id:=seed[0]
+	key:=seed[1]
+	otp := gotp.NewDefaultTOTP(key)
+	value := otp.Now()
+	log.Printf("ID: %v, OTP: %s", id, value)
 
-	// odczyt konfiguracji
-	readFile(&cfg)
-	// odczyt polecen
-	readCommands()
-	key, paramnumbers := Generate_Key()
-	log.Printf("key: %v", base32.StdEncoding.EncodeToString(key[:]))
-	otp := gotp.NewDefaultTOTP(base32.StdEncoding.EncodeToString(key[:]))
-	value := otp.At(0)
-	log.Printf("ID: %v, paramnumbers:%v, OTP: %s", cfg.ID, paramnumbers, value)
+
+
+
 	attrs, _:=readLines(attrsPath)
-	message := Auth{cfg.ID, paramnumbers, value, attrs}
+	message := Auth{id, value, attrs}
 	jsonMessage, _ := json.Marshal(message)
 	fmt.Printf("%s\n", string(jsonMessage))
 	var conn *grpc.ClientConn
@@ -199,11 +127,11 @@ func main() {
 
 	c := chat.NewChatServiceClient(conn)
 
-	response, err := c.SayHello(context.Background(), &chat.Message{Body: string(jsonMessage)})
+	response2, err := c.SayHello(context.Background(), &chat.Message{Body: string(jsonMessage)})
 	if err != nil {
 		log.Fatalf("Error when calling SayHello: %s", err)
 	}
-	log.Printf("Response from server: %s", response.Body)
-	file,_:=os.Create("new_files/user_keys/"+cfg.ID+".json")//plik tworzony jest z nazwą użytkownika
-	file.WriteString(response.Body)//zapis
+	//log.Printf("Response from server: %s", response2.Body)
+	file,_:=os.Create("new_files/user_keys/"+id+".json")//plik tworzony jest z nazwą użytkownika
+	file.WriteString(response2.Body)//zapis
 }
